fb04bb9a9c8dfd2f38a997142e553c2eedd1d23b
Piyush <piyushlekhak@gmail.com>
2025-12-25 14:25:12 +0000

    completed baseline model training of yolov8m

---
+++ scripts/train_yolov8m_baseline.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+model = YOLO("../models/yolov8m.pt")
+
+model.train(
+    data="../data_yolo/data.yaml",
+    epochs=50,
+    batch=10,
+    imgsz=640,
+    device=0,
+    amp=True,
+    workers=4,
+    project="runs/baseline",
+    name="yolov8m_baseline",
+    seed=42,
+    verbose=True,
+    exist_ok=False,
+)
